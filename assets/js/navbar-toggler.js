12d731da82e96e70b11eba857804243b63303d2c
Kyaw Soe Han <kyawsoehmh@gmail.com>
2022-03-29 22:33:22 +0630

    home and destination

---
+++ assets/js/navbar-toggler.js
@@ -0,0 +1,13 @@
+const btn=document.querySelector('.navigation-toggler-btn');
+  const navbar=document.querySelector('.navigation-bar');
+  btn.addEventListener('click',()=>{
+    const visibility= navbar.getAttribute('data-visible');
+    console.log(visibility);
+    if(visibility==="false"){
+      navbar.setAttribute('data-visible',true);
+      btn.setAttribute('aria-expanded',true);
+    }else{
+      navbar.setAttribute('data-visible',false);
+      btn.setAttribute('aria-expanded',false);
+    }
+  });
