6f700d624c1d8272587371865dfff08cae5e2c1a
Kyaw Soe Han <kyawsoehmh@gmail.com>
2022-04-04 23:48:03 +0630

    added click outside navbar close function

--- assets/js/navbar-toggler.js
+++ assets/js/navbar-toggler.js
@@ -1,5 +1,5 @@
 const btn=document.querySelector('.navigation-toggler-btn');
-  const navbar=document.querySelector('.navigation-bar');
+const navbar=document.querySelector('#navigation-bar');
   btn.addEventListener('click',()=>{
     const visibility= navbar.getAttribute('data-visible');
     console.log(visibility);
@@ -7,6 +7,12 @@ const btn=document.querySelector('.navigation-toggler-btn');
       navbar.setAttribute('data-visible',true);
       btn.setAttribute('aria-expanded',true);
     }else{
+      navbar.setAttribute('data-visible',false);
+      btn.setAttribute('aria-expanded',false);
+    }
+  });
+  document.addEventListener('click',(e)=>{
+    if(e.target.id !== 'navigation-bar' && e.target.id !=='navigation-toggler-btn'){
       navbar.setAttribute('data-visible',false);
       btn.setAttribute('aria-expanded',false);
     }
